bfb3ddbbaeb91b48d4d147ee4656b205bc0028a3
traulfs <traulfs@taskit.de>
2024-07-06 13:59:31 +0200

    05.07.24

--- server.go
+++ server.go
@@ -99,7 +99,7 @@ func (s *Server) serv() {
 					}
 					for i := range td.Payload {
 						s.Jack[td.Ch[0]].ReadChan[td.Typ[0]] <- td.Payload[i]
-						fmt.Printf("%x\n", td.Payload[i])
+						fmt.Printf("Ch: %d, Typ: %d, Data: %02x\n", td.Ch[0], td.Typ[0], td.Payload[i])
 					}
 				}
 			case <-s.done:
